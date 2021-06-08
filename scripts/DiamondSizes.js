d5d39f438f511a030df7dcf3e113110e03257614
Peter Racine Jr <peterracinejr@gmail.com>
2021-06-08 15:43:19 -0500

    i THINK i fixed all of the initial bugs aside from getting the button to work correctly. But the page is no longer broken

---
+++ scripts/DiamondSizes.js
@@ -0,0 +1,21 @@
+import { getSizes } from "./database.js";
+
+const sizes = getSizes();
+
+document.addEventListener("change", (event) => {});
+
+export const DiamondSizes = () => {
+  let html = "<ul>";
+
+  // Use .map() for converting objects to <li> elements
+  const listItems = sizes.map((size) => {
+    return `<li>
+            <input type="radio" name="size" value="${size.id}" /> ${size.carets}
+        </li>`;
+  });
+
+  html += listItems.join("");
+  html += "</ul>";
+
+  return html;
+};
---
+++ scripts/JewelryStyles.js
@@ -0,0 +1,22 @@
+import { getStyles } from "./database.js";
+
+const styles = getStyles();
+
+document.addEventListener("change", (event) => {});
+
+export const JewelryStyles = () => {
+  let html = "<ul>";
+
+  // Use .map() for converting objects to <li> elements
+  const listItemsArray = styles.map((style) => {
+    return `<li>
+            <input type="radio" name="style" value="${style.style}" /> ${style.style}
+        </li>`;
+  });
+
+  // Join all of the strings in the array into a single string
+  html += listItemsArray.join(" ");
+
+  html += "</ul>";
+  return html;
+};
