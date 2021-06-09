951c0f2a5261b863600b18c966fb5fcda16dd2b4
Peter Racine Jr <peterracinejr@gmail.com>
2021-06-09 10:37:42 -0500

    added renders and added change event for metal selections by user

--- scripts/Metals.js
+++ scripts/Metals.js
@@ -1,24 +1,23 @@
-import { getMetals } from "./database.js"
+import { getMetals } from "./database.js";
 
-const metals = getMetals()
+const metals = getMetals();
 
-document.addEventListener(
-    "change",
-    (event) => {
-    }
-)
+document.addEventListener("change", (event) => {
+  if (event.target.name === "metal") {
+    window.alert(`User chose metal ${event.target.value}`);
+  }
+});
 
 export const Metals = () => {
-    let html = "<ul>"
+  let html = "<ul>";
 
-    // This is how you have been converting objects to <li> elements
-    for (const metal of metals) {
-        html += `<li>
+  // This is how you have been converting objects to <li> elements
+  for (const metal of metals) {
+    html += `<li>
             <input type="radio" name="metal" value="${metal.id}" /> ${metal.metal}
-        </li>`
-    }
+        </li>`;
+  }
 
-    html += "</ul>"
-    return html
-}
-
+  html += "</ul>";
+  return html;
+};
